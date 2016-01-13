34dcb3455a3e502735b329ddff575973ef52c0f3
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-01-13 23:06:12 +0000

    removed pair parameters

--- src/model/User.js
+++ src/model/User.js
@@ -1,8 +1,6 @@
 var User = function (parameters) {
     this.name = parameters.name;
     this.email = parameters.email;
-    this.currentPair = "";
-    this.previousPair = "";
 };
 
 module.exports = User;
--- test/model/UserTest.js
+++ test/model/UserTest.js
@@ -8,15 +8,11 @@ describe('User', function () {
     describe('#init', function () {
         user = new User({
             name: "first, last",
-            email: "email",
-            currentPair: "",
-            previousPair: ""
+            email: "email"
         });
-        it('should initialize with name, location', function () {
+        it('should initialize with name, email', function () {
             user.name.should.equal("first, last");
             user.email.should.equal("email");
-            user.currentPair.should.equal("");
-            user.previousPair.should.equal("");
         });
     });
 });
